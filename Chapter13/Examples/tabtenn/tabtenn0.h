aa619e3f8ada42b930362e625d0ce6a611bc6f04
kevinrsx <kevinlgc@foxmail.com>
2018-04-15 16:51:17 +0800

    initial commit

---
+++ Chapter13/Examples/tabtenn/tabtenn0.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+using std::string;
+//simple base class
+class TableTennisPlayer
+{
+private:
+	string firstname;
+	string lastname;
+	bool hasTable;
+public:
+	TableTennisPlayer(const string & fn = "none", const string & ln = "none", bool ht = false);
+	void Name() const;
+	bool HasTable() const { return hasTable; }
+	void ResetTable(bool v) { hasTable = v; }
+};
