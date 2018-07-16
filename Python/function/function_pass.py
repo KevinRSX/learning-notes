a4c17e1e4db44c60eeb6cae16e484f33a6e67fa7
kevinrsx <kevinlgc@foxmail.com>
2018-07-16 21:01:14 +0800

    more function

--- Python/function/function_pass.py
+++ Python/function/function_pass.py
@@ -5,4 +5,23 @@ def greet(names):
         print(msg)
 
 usernames = ['hannah', 'ty', 'margot']
-greet(usernames)
+# greet(usernames)
+
+def print_models(unprinted_designs, completed_designs):
+    """simulate to print all designs until there are no unprinted works left
+    move each design to the new list after they are printed"""
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print("Printing model: " + current_design)
+        completed_designs.append(current_design)
+
+def show_completed_models(completed_models):
+    """show all the printed models"""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
